60a8c6fd18c6de599c59b9cb0be6f1b9a534c1ba
seungwooKim99 <sonicdx886@gmail.com>
2021-02-27 11:23:24 +0900

    fixed untracked files

--- config.py
+++
@@ -1,8 +0,0 @@
-db = {
-    'user'     : 'sonic886',
-    'password' : 'rlatmddn52!',
-    'host'     : 'music-recommender-db.cpabptw8fwxo.us-east-2.rds.amazonaws.com',
-    'port'     : '3306',
-    'database' : 'music_recommender_db'
-}
-DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8" 
