3cb83d1c4742e8a0fb65aaa19439cec40356c2e7
seungwooKim99 <sonicdx886@gmail.com>
2021-02-28 20:51:17 +0900

    post 요청시 sql구문으로 db조회후 response

--- server.py
+++ server.py
@@ -5,11 +5,11 @@ import numpy as np
 import pandas as pd
 import json
 
-def db_connector():
+def db_connector(value):
     with open('database.json') as json_file:
         dbset = json.load(json_file)
     db_connection_str = "mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8".format(dbset['user'], dbset['password'], dbset['host'], dbset['port'], dbset['database'])
-    sql = "SELECT id, artists, name FROM original_table LIMIT 10;"
+    sql = "SELECT id, song_id, artists, name FROM original_table WHERE name='{}';".format(value)
     db_connection = create_engine(db_connection_str)
     result = pd.read_sql(sql, con=db_connection)
     return result
@@ -22,6 +22,15 @@ def index():
     # a = pd.DataFrame(a)
     testJson = a.to_json(orient='records')
     return testJson
+
+@app.route('/api/musicL', methods=['POST'])
+def post():
+    value = request.form['songName']
+    print(value)
+    result = db_connector(value)
+    print(result)
+    textJson = result.to_json(orient='records')
+    return textJson
 
 if __name__ == "__main__":
     #app.run(host="0.0.0.0", port="8000")
